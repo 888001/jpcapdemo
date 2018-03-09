1117d1575689cc5cc4e8eacecfbf607277c19d28
胡鹏鹏 <1171132590@qq.com>
2018-03-09 16:38:42 +0800

    Initial commit

---
+++ src/main/java/com/cittt/jpcapdemo/JpcapdemoApplication.java
@@ -0,0 +1,12 @@
+package com.cittt.jpcapdemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JpcapdemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JpcapdemoApplication.class, args);
+	}
+}
